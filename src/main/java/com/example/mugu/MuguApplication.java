686561a6c541983f57ee29890dbcbc534fcd6011
lhs991117 <lhs991117@gmail.com>
2023-10-28 13:46:35 +0900

    mugu start

---
+++ src/main/java/com/example/mugu/MuguApplication.java
@@ -0,0 +1,13 @@
+package com.example.mugu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mugu/MuguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mugu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
